22078dcbd657286511b7229bcaabb45ec4970163
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-02-26 20:53:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,58 +11,8 @@ import WorldHexagonMap1 from "./pages/WorldHexagonMap1";
 import { useEffect } from "react";
 
 function App() {
-  // const action = useNavigationType();
-  // const location = useLocation();
-  // const pathname = location.pathname;
-
-  // useEffect(() => {
-  //   if (action !== "POP") {
-  //     window.scrollTo(0, 0);
-  //   }
-  // }, [action, pathname]);
-
-  // useEffect(() => {
-  //   let title = "";
-  //   let metaDescription = "";
-
-  //   switch (pathname) {
-  //     case "/":
-  //       title = "";
-  //       metaDescription = "";
-  //       break;
-  //     case "/bubble-chart":
-  //       title = "";
-  //       metaDescription = "";
-  //       break;
-  //     case "/world-hexagon-map":
-  //       title = "";
-  //       metaDescription = "";
-  //       break;
-  //   }
-
-  //   if (title) {
-  //     document.title = title;
-  //   }
-
-  //   if (metaDescription) {
-  //     const metaDescriptionTag = document.querySelector(
-  //       'head > meta[name="description"]'
-  //     );
-  //     if (metaDescriptionTag) {
-  //       metaDescriptionTag.content = metaDescription;
-  //     }
-  //   }
-  // }, [pathname]);
-
   return (
-  //   <Routes>
-  //     <Route path="/" element={<WorldHexagonMap />} />
-
-  //     <Route path="/bubble-chart" element={<BubbleChart />} />
-
-  //     <Route path="/world-hexagon-map" element={<WorldHexagonMap1 />} />
-  
-  //   </Routes>
+ 
   <div>
     <WorldHexagonMap />
     <WorldHexagonMap1 />
